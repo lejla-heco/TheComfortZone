ac74ca4e78596c6a569c4fba1154006df4ec54d6
lejlaheco <hecolejla@hotmail.com>
2022-06-06 06:11:17 +0200

    Added home page image into Resources folder of the project

--- TheComfortZone.WINUI/Forms/HomePage/frmHomePage.Designer.cs
+++ TheComfortZone.WINUI/Forms/HomePage/frmHomePage.Designer.cs
@@ -28,14 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHomePage));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Image = global::TheComfortZone.WINUI.Properties.Resources.HomePage;
             this.pictureBox1.Location = new System.Drawing.Point(-2, 0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(939, 542);
--- TheComfortZone.WINUI/Properties/Resources.Designer.cs
+++ TheComfortZone.WINUI/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace TheComfortZone.WINUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap HomePage {
+            get {
+                object obj = ResourceManager.GetObject("HomePage", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap open_eye {
             get {
                 object obj = ResourceManager.GetObject("open-eye", resourceCulture);
